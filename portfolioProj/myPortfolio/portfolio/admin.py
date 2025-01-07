811f150a46ed89d991d3c9c7dbb036dc91a3d05d
Divyamaan <110406163+Divyamaan69@users.noreply.github.com>
2025-01-07 23:42:07 +0530

    Delete portfolioProj directory

--- portfolioProj/myPortfolio/portfolio/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import TechExample, WritingExample
-
-admin.site.register(TechExample)
-admin.site.register(WritingExample)
