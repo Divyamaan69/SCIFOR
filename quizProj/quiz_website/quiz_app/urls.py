2dd3b1e696375f218fad1731b6e1a8b335c50068
Divyamaan <110406163+Divyamaan69@users.noreply.github.com>
2025-01-07 23:42:54 +0530

    Delete quizProj directory

--- quizProj/quiz_website/quiz_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('quiz/', views.start_quiz, name='start_quiz'),
-    path('submit/', views.submit_quiz, name='submit_quiz'),
-    path('history/', views.history, name='history'),
-]
