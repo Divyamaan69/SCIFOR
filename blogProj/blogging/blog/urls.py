3dcbbf48fe14226e3b6529888207610958c76c54
Divyamaan <110406163+Divyamaan69@users.noreply.github.com>
2025-01-07 23:37:30 +0530

    Delete blogProj directory

--- blogProj/blogging/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('add/', views.add_post, name='add_post'),
-]
